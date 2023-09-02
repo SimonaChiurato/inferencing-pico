120a12a38c76924a62fb5275334eb24c43e1f8bd
Simona Chiurato <61753013+SimonaChiurato@users.noreply.github.com>
2023-09-02 18:14:44 +0200

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -77,12 +77,12 @@ while(true) {
 
         for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
             ei_printf("    %s: %.5f\n", result.classification[ix].label, result.classification[ix].value);
-            
+            if (result.classification[ix].value > 0.5 && true_results[sampleIdx] == ix) { 
+                correct_predictions++; 
+            }
         }
         ei_printf(" (True result: %.1f)", true_results[ix]);
-        if (result.classification[ix].value > 0.5 && true_results[sampleIdx] == ix) { 
-        correct_predictions++; 
-    }
+        
 #if EI_CLASSIFIER_HAS_ANOMALY == 1
         ei_printf("    anomaly score: %.3f\n", result.anomaly);
 #endif
